0879b48d83f3a329297d39ac54dd95b6dd757724
Anand <anandaralikatti6@gmail.com>
2026-06-22 19:37:12 +0530

    Add files via upload

---
+++ pythonD2.py
@@ -0,0 +1,478 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+print('a')
+
+
+# In[3]:
+
+
+print("a")
+
+
+# In[4]:
+
+
+# i want to take input
+# user input
+# dont 
+# add two number
+a=9
+b=10
+a=b
+
+
+# In[5]:
+
+
+a= input('enter the num')
+type(a)
+
+
+# In[10]:
+
+
+a= input('enter the num')
+b= input('enter the num')
+a+b
+
+
+# In[9]:
+
+
+a= int(input('enter the num'))
+b= int(input('enter the num'))
+a+b
+
+
+# In[11]:
+
+
+a= int(input('enter the num'))
+b= int(input('enter the num'))
+c=a+b
+print('The answer is',c)
+
+
+# In[ ]:
+
+
+# take the user input
+# do multiplication
+# multiply two number
+
+
+# In[12]:
+
+
+a= int(input('enter the num'))
+b= int(input('enter the num'))
+c=a*b
+print('The answer is',c)
+
+
+# In[14]:
+
+
+a= float(input('enter the num'))
+b= float(input('enter the num'))
+c=a*b
+print('The answer is',c)
+
+
+# In[15]:
+
+
+a= float(input('enter the num'))
+b= float(input('enter the num'))
+c=a*b
+print('The answer is',c)
+
+
+# In[16]:
+
+
+a= float(input('enter the num'))
+b= float(input('enter the num'))
+c=a/b
+print('The answer is',c)
+
+
+# In[17]:
+
+
+a= float(input('enter the num'))
+b= float(input('enter the num'))
+c=a**b
+print('The answer is',c)
+
+
+# In[19]:
+
+
+c
+
+
+# In[20]:
+
+
+a= eval(input('enter the num'))
+b= eval(input('enter the num'))
+c=a*b
+print('The answer is',c)
+
+
+# In[ ]:
+
+
+# u have two swap the number
+a=9
+b=6
+# after swapping
+a=6
+b=9
+
+
+# In[23]:
+
+
+a=int(input('enter 1st num'))
+b=int(input('enter 2st num'))
+a=a+b
+b=a-b
+a=a-b
+print('the swapping is',a)
+print('the swapping is',b)
+
+
+# In[1]:
+
+
+a=9
+b=6
+a,b=b,a
+print(a,b)
+
+
+# In[27]:
+
+
+# string
+a='python'
+# indexing
+a[5]
+
+
+# In[28]:
+
+
+# string
+a='python'
+# indexing
+a[100]
+
+
+# In[31]:
+
+
+a=['welcome to python']
+print(a)
+
+
+# In[34]:
+
+
+a='welcome to python'
+a[7]
+
+
+# In[35]:
+
+
+a='welcome to python'
+a[-8]
+
+
+# In[36]:
+
+
+# slicing
+a='welcome to python'
+a[0:6]
+
+
+# In[37]:
+
+
+# slicing
+a='welcome to python'
+a[-1:-6]
+
+
+# In[41]:
+
+
+# slicing
+a='welcome to python'
+a[11:13]
+
+
+# In[42]:
+
+
+# slicing
+a='welcome to python'
+a[15:17]
+
+
+# In[43]:
+
+
+# slicing
+a='welcome to python'
+a[15:]
+
+
+# In[44]:
+
+
+# slicing
+a='welcome to python'
+a[12:]
+
+
+# In[46]:
+
+
+a='welcome to python'
+a[:7]
+
+
+# In[48]:
+
+
+a='welcome to python'
+a[0:7:2]
+
+
+# In[49]:
+
+
+a='python'
+a[0:6:4]
+
+
+# In[50]:
+
+
+a='python'
+a[0:6:3]
+
+
+# In[51]:
+
+
+a='python'
+a[0:6:5]
+
+
+# In[52]:
+
+
+a='welcome to python'
+a[0:16:12]
+
+
+# In[53]:
+
+
+a='welcome to python'
+a[2:16:12]
+
+
+# In[54]:
+
+
+a='welcome to python'
+a[2:55:12]
+
+
+# In[2]:
+
+
+a='LearningChampion'
+a[8:13:1]
+
+
+# In[3]:
+
+
+a='LearningChampion'
+a[4:8:1]
+
+
+# In[4]:
+
+
+a='LearningChampion'
+a[6:10:1]
+
+
+# In[8]:
+
+
+a='LearningChampion'
+a[::1]
+
+
+# In[9]:
+
+
+a='LearningChampion'
+a[::2]
+
+
+# In[10]:
+
+
+a='LearningChampion'
+a[::3]
+
+
+# In[11]:
+
+
+a='LearningChampion'
+a[0:6:3]
+
+
+# In[13]:
+
+
+a='LearningChampion'
+a[0:6:4]
+
+
+# In[14]:
+
+
+a='LearningChampion'
+a[8:15:3]
+
+
+# In[19]:
+
+
+a='AnandAralikatti'
+a[5:30:3]
+
+
+# In[22]:
+
+
+a='AnandAralikatti'
+a[::3]
+type(a)
+
+
+# In[7]:
+
+
+a='LearningChampion'
+a[6:10:2]
+
+
+# In[ ]:
+
+
+# slicing using -ve index
+# no
+
+
+# In[55]:
+
+
+a='welcome to python'
+a[-1:-3:-1]
+
+
+# In[64]:
+
+
+a='welcome to python'
+a[-10:-15:-1]
+
+
+# In[81]:
+
+
+a='welcome to python'
+a[-5:-3]
+
+
+# In[83]:
+
+
+a='welcome to python'
+a[::-1]
+
+
+# In[84]:
+
+
+# inbulid method in str
+# i want to check length of the string
+len(a)
+
+
+# In[85]:
+
+
+min(a)
+
+
+# In[86]:
+
+
+max(a)
+
+
+# In[87]:
+
+
+'mumbai'>'pune'
+
+
+# In[89]:
+
+
+'delhi'=='delhi'
+
+
+# In[90]:
+
+
+'delhi'=='delhi'
+# asci value
+
+
+# In[91]:
+
+
+sorted(a)
+
+
+# In[92]:
+
+
+sorted(a,reverse=True)
+
+
+# In[ ]:
+
+
+
+
